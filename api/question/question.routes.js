aabf0600943cf6c856cfabb3968cab015006c5fa
Bhavin Karad <bhavinkarad@gmail.com>
2018-10-29 19:12:18 +0530

    add required validator

--- api/question/question.routes.js
+++ api/question/question.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 const controller = require('./question.controller');
+const validator = require('./question.validator');
 
 /** all router for question controller */
 router.get('/', controller.getQuestionsList);
 router.get('/:id', controller.getQuestionById);
-router.post('/', controller.addQuestion);
+router.post('/', [...validator.validateAddQuestion, validator.customValidator], controller.addQuestion);
 router.post('/:id', controller.updateQuestion);
 router.post('/bulk', controller.addMultipleQuestions);
 router.delete('/:id', controller.deleteQuestion);
---
+++ api/question/question.validator.js
@@ -0,0 +1,48 @@
+const { check, validationResult } = require('express-validator/check');
+
+const validator = {
+  customValidator: (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  validateAddQuestion: [
+    check('categories_COD')
+      .exists().withMessage('categories_COD is required'),
+    check('category')
+      .exists()
+      .withMessage('category is required!'),
+    check('topic_Cod')
+      .exists()
+      .withMessage('topic_Cod is required!'),
+    check('subCat')
+      .exists()
+      .withMessage('subCat is required!'),
+    check('question_Cod')
+      .exists()
+      .withMessage('question_Cod is required!'),
+    check('question')
+      .exists()
+      .withMessage('question is required!'),
+    check('options')
+      .exists()
+      .withMessage('options is required!')
+      .isArray()
+      .withMessage('options is Array'),
+    check('options.*.answer')
+      .exists()
+      .withMessage('options answer is required!'),
+    check('options.*.code')
+      .exists()
+      .withMessage('options code is required!'),
+    check('options.*.score')
+      .exists()
+      .withMessage('options score is required!')
+      .isNumeric()
+      .withMessage('score must be a number'),
+  ],
+};
+
+module.exports = validator;
